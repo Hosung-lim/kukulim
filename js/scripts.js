7ad4a67f7efa4015a03af575360af87266e6a57f
Hosung-lim <72371057+Hosung-lim@users.noreply.github.com>
2022-05-23 00:10:54 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', event => {
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            offset: 40,
+            offset: 10,
         });
     };
 
